7a33671796d45e35d9b1d54c609ba1be0af3153f
Lucia Belen <lbdelilla@gmail.com>
2023-09-14 16:18:58 +0200

    change: version of engine node again d

--- Frontend/src/pages/CreatePost.jsx
+++ Frontend/src/pages/CreatePost.jsx
@@ -22,13 +22,16 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true)
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        })
+        const response = await fetch(
+          'https://dalle-2-0-26r1.onrender.com/api/v1/dalle',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        )
 
         if (response.status === 429) {
           toast.error(
@@ -90,13 +93,16 @@ const CreatePost = () => {
     if (form.name && form.prompt && form.photo) {
       setLoading(true)
       try {
-        const response = await fetch('http://localhost:8080/api/v1/posts', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ ...form }),
-        })
+        const response = await fetch(
+          'https://dalle-2-0-26r1.onrender.com/api/v1/posts',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ ...form }),
+          }
+        )
         await response.json()
         navigate('/')
       } catch (error) {
